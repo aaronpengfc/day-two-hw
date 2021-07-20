1c8c472199d4de04f0623c2a3167182e00d60ef5
aaronpengfc <87631336+aaronpengfc@users.noreply.github.com>
2021-07-20 20:57:17 +0800

    Add files via upload

---
+++ day2 hw1.py
@@ -0,0 +1,7 @@
+score = input("成績")
+score = int(score)
+if score >=60:
+    print("pass")
+else:
+    print("fail")
+    
---
+++ day2 hw3.py
@@ -0,0 +1,10 @@
+math = input("math score")
+math = int(math)
+english = input("english score")
+english = int(english)
+if english >=90 and math >=90:
+    print("money")
+elif english ==100 or math ==100:
+    print("money")
+else:
+    print("no money")
